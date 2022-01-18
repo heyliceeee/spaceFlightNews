abee8c7c53956a7c9a6eeaa7f38f9d3379403231
heyliceeee <alicedias2002@hotmail.com>
2022-01-18 09:54:30 +0000

    fix articledetailsviewcontroller

---
+++ spaceNewsApi/today/ArticleDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleDetailsViewController.swift
+//  spaceNewsApi
+//
+//  Created by Alice Dias on 18/01/2022.
+//
+
+import UIKit
+
+class ArticleDetailsViewController: UIViewController {
+    
+    static let identifier = "ArticleDetailsViewController"
+}
